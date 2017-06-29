3a4921227f25b2d7a627bab9bf5708c4dd988a20
David Alaniz <davidsgretsch@gmail.com>
2017-06-28 23:09:56 -0300

    Fase 02 #3

--- JS_AVANZADO-ARCHIVOS_FASE_INICIAL/js/contact.js
+++ JS_AVANZADO-ARCHIVOS_FASE_INICIAL/js/contact.js
@@ -5,64 +5,66 @@ var user_name = document.querySelector('input[name="nombre"]');
 user_name.addEventListener('blur', nameValidation);
 
 function nameValidation (event) {
-	removeClass(this);
-	if (this.value.length > 4 && isNaN(this.value.charAt(0))) {
-		this.classList.add('ok');
+	clearClass(user_name);
+	if (user_name.value.length > 4 && isNaN(user_name.value.charAt(0))) {
+		user_name.classList.add('ok');
 		return true;
 	} else {
-		this.classList.add('error');
+		user_name.classList.add('error');
 		return false;
 	}
 };
 
-//Exercise 3
+// Exercise 3
 
 var user_email = document.querySelector('input[name="email"]');
 user_email.addEventListener('blur', emailValidation);
 
 function emailValidation (event) {
-	removeClass(this);
-	if (this.value.indexOf('@') != -1 && this.value.indexOf('.') != -1) {
-		this.classList.add('ok');
+	clearClass(user_email);
+	if (user_email.value.indexOf('@') != -1 && user_email.value.indexOf('.') != -1) {
+		user_email.classList.add('ok');
 		return true;
 	} else {
-		this.classList.add('error');
+		user_email.classList.add('error');
 		return false;
 	}
 };
 
-//Exercise 4
+// Exercise 4
+
 var user_msg = document.querySelector('textarea[name="mensaje"]');
 user_msg.addEventListener('blur', msgValidation);
 
 function msgValidation (event) {
-	removeClass(this);
-	if (this.value.length && this.value.length < 200) {
-		this.classList.add('ok');
+	clearClass(user_msg);
+	if (user_msg.value && user_msg.value.length < 200) {
+		user_msg.classList.add('ok');
 		return true;
 	} else {
-		this.classList.add('error');
+		user_msg.classList.add('error');
 		return false;
 	}
 };
 
-function removeClass (element) {
-	element.classList.remove('ok');
-	element.classList.remove('error');
+function clearClass (elem) {
+	elem.classList.remove('ok', 'error');
 };
-//Exercise 5
-var form = document.querySelector('.contact-form form');
-form.addEventListener('submit', checkValidations);
 
-function checkValidations (event) {
-	//data no send
+// Exercise 5
+
+var form = document.querySelector('.contact-form form');
+form.addEventListener('submit', formSubmit);
+
+function formSubmit (event) {
 	event.preventDefault();
+
 	if (!nameValidation()) {
 		alert('Error! Nombre incompleto o invalido');
 	} else if (!emailValidation()) {
-		alert('Error! Email incompleto o invalido');
+		alert('Error! Email incompleto o invalido');		
 	} else if (!msgValidation()) {
-		alert('Error! Mensaje incompleto o invalido')
+		alert('Error! Mensaje incompleto o invalido');				
 	} else {
 		event.target.submit();
 	}
